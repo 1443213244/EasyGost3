deeb7278e29e0fe4b34f006ee5561ed22a5e1b6a
RexChou <285169606@qq.com>
2023-01-13 19:23:20 +0800

    fix

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mv gost /usr/bin/gost
 chmod -R 777 /usr/bin/gost
-mv config.yaml /root/gost.yaml && chmod -R 777 /root/gost.yaml
+mv config.yaml /gost.yaml && chmod -R 777 /gost.yaml
 chmod -R 777 gost.service && mv gost.service /usr/lib/systemd/system 
 systemctl enable gost && systemctl restart gost
