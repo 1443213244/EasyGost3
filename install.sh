e092f00ad8dce9549c1e4ae478e9a9663ba8e018
1443213244 <1443213244@qq.com>
2024-02-23 16:34:30 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-mv gost /usr/bin/gost
-chmod -R 777 /usr/bin/gost
-mv config.yaml /gost.yaml && chmod -R 777 /gost.yaml
-chmod -R 777 gost.service && mv gost.service /usr/lib/systemd/system 
+
+# 检查 /gost.yaml 是否存在
+if [ -f "/gost.yaml" ]; then
+    echo "/gost.yaml 文件已存在，脚本退出。"
+    exit 1
+fi
+
+# 移动并设置权限
+mv gost /usr/bin/gost && chmod 755 /usr/bin/gost
+mv config.yaml /gost.yaml && chmod 644 /gost.yaml
+mv gost.service /usr/lib/systemd/system/gost.service && chmod 644 /usr/lib/systemd/system/gost.service
+
+# 启用并重启服务
 systemctl enable gost && systemctl restart gost
